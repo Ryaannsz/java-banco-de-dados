c7261bdff94a7816c5e53c782157ec09857d5a09
Ryan Carvalho Bernardo <ryancarvalho.ber@gmail.com>
2024-01-16 21:54:21 -0300

    criação do sistema de empréstimo de livro

--- java-banco-de-dados/src/main/java/br/com/aplicacoesjava/maven/conexao.java
+++ java-banco-de-dados/src/main/java/br/com/aplicacoesjava/maven/conexao.java
@@ -1,11 +1,16 @@
 package br.com.aplicacoesjava.maven;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
+
+
+import com.google.protobuf.TextFormat.ParseException;
 
 public class conexao {
 	//Referência global
@@ -30,6 +35,10 @@ public class conexao {
 				System.out.println("Caso 4 - excluir livro");
 				System.out.println("Caso 5 - listar autor");
 				System.out.println("Caso 6 - listar livros");
+				System.out.println("Caso 7 - criar usuarios");
+				System.out.println("Caso 8- remover usuarios");
+				System.out.println("Caso 9 - listar usuarios");
+				System.out.println("Caso 10 - criar emprestimos");
 				op=sc.nextInt();
 			
 			switch(op) {
@@ -50,6 +59,18 @@ public class conexao {
 					break;
 				case 6:
 					listarLivro();
+					break;
+				case 7:
+					criarUsuario();
+					break;
+				case 8:
+					excluirUsuario();
+					break;
+				case 9:
+					listarUsuarios();
+					break;
+				case 10:
+					criarEmprestimo();
 					break;
 				default:
 					whilezinho++;
@@ -122,6 +143,55 @@ public class conexao {
 	public static void listarLivro() {
 		Livro livro = new Livro(null, 0, conexao);
 		livro.listarLivrosBanco();
+	}
+	
+	public static void criarUsuario() {
+		System.out.println("Digite o nome do usuário: ");
+		String nomeUsuario = sc.next();
+		System.out.println("Digite o CPF: ");
+		String cpfUsuario = sc.next();
+		
+		Usuario usuario = new Usuario(nomeUsuario, cpfUsuario, conexao);
+		usuario.criarUsuario();
+	}
+	
+	public static void excluirUsuario() {
+		listarUsuarios();
+		
+		System.out.println("Digite o nome do usuário que deseja excluir: ");
+		String nomeUsuario = sc.next();
+		
+		Usuario usuario = new Usuario(nomeUsuario, null, conexao);
+		usuario.excluirUsuario();
+	}
+	
+	public static void listarUsuarios() {
+		Usuario usuario = new Usuario(null, null, conexao);
+		usuario.listarUsuarios();
+	}
+	
+	public static void criarEmprestimo() {
+		System.out.println("Qual usuário que deseja pegar um livro?");
+		String nomeUsuario = sc.next();
+		
+		
+		System.out.println("Qual livro "+nomeUsuario+" deseja pegar?");
+		String tituloLivro = sc.next();
+		
+		System.out.println("Qual a data deseja devolver? Formato: dd/MM/yyyy");
+		String inputDate = sc.next();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy");
+		
+		try {
+			
+            java.util.Date utilDate = dateFormat.parse(inputDate);
+            Date sqlDate = new java.sql.Date(utilDate.getTime());
+			
+			Emprestimo emprestimo = new Emprestimo(nomeUsuario, tituloLivro, sqlDate, conexao);
+			emprestimo.criarEmprestimo();
+		} catch (java.text.ParseException e) {
+			System.out.println("Erro ao formatar a data: "+e.getMessage());
+		}
 	}
 	
 	
